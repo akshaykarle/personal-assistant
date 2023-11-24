cfb43347b46c95ba03316358301825629cdb1c61
Akshay Karle <1443108+akshaykarle@users.noreply.github.com>
2023-11-24 10:30:07 +0000

    start loading documents from a directory using extension specific loaders to test

--- app/app.py
+++ app/app.py
@@ -1,18 +1,36 @@
-from langchain.document_loaders.csv_loader import CSVLoader
+import os
+from langchain import document_loaders
+from langchain.docstore.document import Document
 from langchain.embeddings import GPT4AllEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.llms import GPT4All
-from langchain.chains import LLMChain
-from langchain.prompts import PromptTemplate
 from langchain import hub
 from langchain.chains import RetrievalQA
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-csv_loader = CSVLoader(file_path="../data/bank_statements/monzo.csv")
-data = csv_loader.load()
+def load_data(directory: str) -> list[Document]:
+    extension_with_loader = {
+        "csv": document_loaders.CSVLoader,
+        "txt": document_loaders.TextLoader,
+        "html": document_loaders.BSHTMLLoader,
+        "json": document_loaders.JSONLoader,
+        "md": document_loaders.UnstructuredMarkdownLoader,
+    }
+    data = []
+
+    for extension, loader in extension_with_loader.items():
+        print(f"Loading {extension} files from {directory}")
+        text_loader_kwargs={'autodetect_encoding': True}
+        data += document_loaders.DirectoryLoader(path=directory, glob=f"**/*.{extension}", recursive=True, loader_cls=loader, show_progress=True, use_multithreading=True, loader_kwargs=text_loader_kwargs).load()
+        print(f"Loaded {len(data)} {extension} files successfully!")
+
+    print(f"Loaded all {len(data)} files successfully!")
+    return data
+
+docs = load_data("../data")
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
-all_splits = text_splitter.split_documents(data)
+all_splits = text_splitter.split_documents(docs)
 
 vectorstore = Chroma.from_documents(documents=all_splits, embedding=GPT4AllEmbeddings())
 
