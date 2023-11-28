b53db4610bdab478e5342db4225657246dfe3354
Akshay Karle <1443108+akshaykarle@users.noreply.github.com>
2023-11-28 17:08:23 +0000

    ingest and split md docs including notion documents

--- app/ingest.py
+++ app/ingest.py
@@ -1,4 +1,4 @@
-from langchain.document_loaders import CSVLoader, DirectoryLoader, JSONLoader, NotionDirectoryLoader
+from langchain.document_loaders import CSVLoader, DirectoryLoader, JSONLoader
 from langchain.docstore.document import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter, MarkdownHeaderTextSplitter
 
@@ -10,9 +10,9 @@ def split_documents(documents: list[Document]) -> list[Document]:
     return all_splits
 
 
-def load_notion_docs(directory: str) -> list[Document]:
+def load_md_docs(directory: str) -> list[Document]:
     docs = []
-    notion_docs = NotionDirectoryLoader(path=f"{directory}/notion_db").load()
+    md_docs = DirectoryLoader(path=directory, glob="**/*.md", recursive=True, show_progress=True, silent_errors=True, loader_kwargs={"autodetect_encoding": True, "mode": "elements"}).load()
     headers_to_split_on = [
         ("#", "Heading 1"),
         ("##", "Heading 2"),
@@ -22,16 +22,16 @@ def load_notion_docs(directory: str) -> list[Document]:
         ("\\n\\n", "Paragraph"),
     ]
     md_splitter = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_split_on, return_each_line=False)
-    for d in notion_docs:
-        docs += md_splitter.split_text(d.page_content)
+    for d in md_docs:
+        split_docs = md_splitter.split_text(d.page_content)
+        docs += [Document(page_content=doc.page_content, metadata=d.metadata) for doc in split_docs]
     return docs
 
 def load_docs_from_dir(directory: str) -> list[Document]:
     docs = []
     docs = DirectoryLoader(path=directory, glob="**/*.json", recursive=True, show_progress=True, silent_errors=True, loader_cls=JSONLoader, loader_kwargs={"jq_schema": ".", "text_content": False}).load()
     docs += DirectoryLoader(path=directory, glob="**/*.csv", recursive=True, show_progress=True, silent_errors=True, loader_cls=CSVLoader, loader_kwargs={"csv_args":{"delimiter": "\n"}}).load()
-    # load all non-json/csv files as text
-    # TODO: make sure this doesn't load notion data
+    docs += load_md_docs(directory)
+    # load all non-json/csv/md files as text
     docs += DirectoryLoader(path=directory, glob="**/*[!(.json|.csv|.md)]", recursive=True, show_progress=True, silent_errors=True, loader_kwargs={"autodetect_encoding": True, "mode": "elements"}).load()
-    docs += load_notion_docs(f"{directory}/notion_db")
     return split_documents(docs)
