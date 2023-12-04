98daf4023400282db1f337144a102e6d91c69619
Akshay Karle <1443108+akshaykarle@users.noreply.github.com>
2023-12-04 13:39:07 +0000

    fix flake to work with linux

--- flake.nix
+++ flake.nix
@@ -15,26 +15,31 @@
       let
         # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
         pkgs = nixpkgs.legacyPackages.${system};
-        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
-      in
-      {
+        nativeBuildInputs = with pkgs; [
+          stdenv
+          python310
+          poetry
+          tesseract # needed by unstructured.io to parse pdf and image files
+          poppler_utils # needed by unstructured.io to parse pdf files
+          pandoc # needed by unstructured.io to parse EPUBs, RTFs and Open Office docs
+        ];
+        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; })
+          mkPoetryApplication;
+      in {
+        inherit nativeBuildInputs;
+
         packages = {
           myapp = mkPoetryApplication {
             projectDir = self;
-            python = pkgs.python311;
+            python = pkgs.python310;
           };
           default = self.packages.${system}.myapp;
         };
 
         devShells.default = pkgs.mkShell {
           # inputsFrom = [ self.packages.${system}.myapp ];
-          packages = with pkgs; [
-            python311
-            poetry
-            tesseract # needed by unstructured.io to parse pdf and image files
-            poppler_utils # needed by unstructured.io to parse pdf files
-            pandoc # needed by unstructured.io to parse EPUBs, RTFs and Open Office docs
-          ];
+          LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+          packages = nativeBuildInputs;
         };
       });
 }
