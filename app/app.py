b139c1937b9af929815a5bacb851b4742d131d12
Akshay Karle <1443108+akshaykarle@users.noreply.github.com>
2023-12-05 17:53:23 +0000

    add chatbot with history and use streamlit

--- app/app.py
+++ app/app.py
@@ -1,17 +1,75 @@
 from pprint import pprint
+import os
 from db import Database
 from ingest import load_docs_from_dir
 from langchain.llms import LlamaCpp
-from langchain.prompts import PromptTemplate
-from langchain.chains import RetrievalQA
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
+import streamlit as st
+
+st.set_page_config(page_title="Personal Assistant", page_icon="🕵️‍♂️")
+st.title("🕵️ Personal Assistant: Personalised help with daily tasks")
+
+
+# @st.cache_resource(ttl="1h")
+def configure_retriever(db: Database):
+    # Read documents
+    if db.is_empty():
+        print("Database is empty, loading documents...")
+        docs = load_docs_from_dir("../data")
+        db.store_documents(docs)
+    else:
+        print("Database is ready, preparing retriever...")
+
+    retriever = db.docstore.as_retriever(search_type="similarity", search_kwargs={"k": 6, "fetch_k": 100, "lambda_mult": 0.50})
+
+    return retriever
+
+
+class StreamHandler(BaseCallbackHandler):
+    def __init__(self, container: st.delta_generator.DeltaGenerator, initial_text: str = ""):
+        self.container = container
+        self.text = initial_text
+        self.run_id_ignore_token = None
+
+    def on_llm_start(self, serialized: dict, prompts: list, **kwargs):
+        # Workaround to prevent showing the rephrased question as output
+        if prompts[0].startswith("Human"):
+            self.run_id_ignore_token = kwargs.get("run_id")
+
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        if self.run_id_ignore_token == kwargs.get("run_id", False):
+            return
+        self.text += token
+        self.container.markdown(self.text)
+
+
+class PrintRetrievalHandler(BaseCallbackHandler):
+    def __init__(self, container):
+        self.status = container.status("**Context Retrieval**")
+
+    def on_retriever_start(self, serialized: dict, query: str, **kwargs):
+        self.status.write(f"**Question:** {query}")
+        self.status.update(label=f"**Context Retrieval:** {query}")
+
+    def on_retriever_end(self, documents, **kwargs):
+        for idx, doc in enumerate(documents):
+            source = os.path.basename(doc.metadata["source"])
+            self.status.write(f"**Document {idx} from {source}**")
+            self.status.markdown(doc.page_content)
+        self.status.update(state="complete")
+
 
 db = Database(collection_name="personal_assistant")
-if db.is_empty():
-    print("Database is empty, loading documents...")
-    docs = load_docs_from_dir("../data")
-    db.store_documents(docs)
+retriever = configure_retriever(db)
 
+# Setup memory for contextual conversation
+msgs = StreamlitChatMessageHistory()
+memory = ConversationBufferMemory(memory_key="chat_history", chat_memory=msgs, return_messages=True)
 
+# Setup LLM and QA chain
 llm = LlamaCpp(
     model_path="../models/llama-cpp/llama-2-13b.Q4_K_M.gguf",
     temperature=0.75,
@@ -21,36 +79,25 @@ llm = LlamaCpp(
     n_ctx=3000,
 )
 
-retriever = db.docstore.as_retriever(search_type="similarity", search_kwargs={"k": 6, "fetch_k": 100, "lambda_mult": 0.50})
-
-prompt_template = """
-You are an assistant working for Sahaj Software Consultancy for specialized for question-answering tasks. Use the following pieces of retrieved context from the Sahaj database to answer the question. If you don't know the answer, just say that you don't know. Use five sentences maximum and keep the answer concise.
-Question: {question}
-Context: {context}
-Answer:"""
-
-rag_prompt = PromptTemplate(
-    template=prompt_template, input_variables=["context", "question"]
-)
-
-qa = RetrievalQA.from_chain_type(
+qa_chain = ConversationalRetrievalChain.from_llm(
     llm=llm,
-    # chain_type="map_reduce",
-    return_source_documents=True,
     retriever=retriever,
-    chain_type_kwargs={"prompt": rag_prompt},
+    memory=memory,
+    verbose=True,
 )
 
-query2 = "Have we done any work in the financial industry? If so, which client did we work for?"
-relevant_docs = retriever.get_relevant_documents(query2)
-print(len(relevant_docs))
-pprint(relevant_docs)
-pprint([d.metadata['source'] for d in relevant_docs])
-response = qa(query2)
-pprint(f"""
-        Question: {response['query']}
-        Answer: {response['result']}
+if len(msgs.messages) == 0 or st.sidebar.button("Clear message history"):
+    msgs.clear()
+    msgs.add_ai_message("How can I help you?")
 
-        No. of Source documents: {len(response['source_documents'])}
-        Source documents: {response['source_documents']}
-""")
+avatars = {"human": "user", "ai": "assistant"}
+for msg in msgs.messages:
+    st.chat_message(avatars[msg.type]).write(msg.content)
+
+if user_query := st.chat_input(placeholder="Ask me anything!"):
+    st.chat_message("user").write(user_query)
+
+    with st.chat_message("assistant"):
+        retrieval_handler = PrintRetrievalHandler(st.container())
+        stream_handler = StreamHandler(st.empty())
+        response = qa_chain.run(user_query, callbacks=[retrieval_handler, stream_handler])
